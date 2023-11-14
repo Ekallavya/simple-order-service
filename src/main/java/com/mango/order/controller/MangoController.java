b5c9a29e906c719e90fea5841bd73c1e2a1c0f80
madhureddi <madhureddie@gmail.com>
2023-11-14 10:58:42 +0530

    removed .properties and added yaml file

--- src/main/java/com/mango/order/controller/MangoController.java
+++ src/main/java/com/mango/order/controller/MangoController.java
@@ -10,28 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mango.order.domain.Mango;
 
-
-
-
 @RestController
 public class MangoController {
-	
-	
-	private static final Map<Integer, Mango> mangoData = new HashMap<Integer, Mango>() {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = -3970206781360313502L;
 
-		{
-			put(1,new Mango(1,"Banish"));
-			put(2,new Mango(2,"Ratnagiri"));
-			put(3,new Mango(3,"Sindhuri"));
-			put(4,new Mango(4,"Thotapuri"));
-			put(5,new Mango(5,"Kobban"));
-		}
- 
-    };
+	private static final long serialVersionUID = -3970206781360313502L;
+	private static final Map<Integer, Mango> mangoData = getMangoData();
  
     @RequestMapping(value = "/mango/{Id}", method = RequestMethod.GET)
     public Mango getEmployeeDetails(@PathVariable int Id) {
@@ -47,4 +30,23 @@ public class MangoController {
         return mango;
     }
 
+
+	private static Map<Integer, Mango> getMangoData() {
+		Map<Integer, Mango> mangoData = new HashMap<Integer, Mango>() ;
+
+		mangoData.put(1, new Mango(1, "Banish"));
+		mangoData.put(2, new Mango(2, "Alphonso"));
+		mangoData.put(3, new Mango(3, "Sindhuri"));
+		mangoData.put(4, new Mango(4, "Totapuri"));
+		mangoData.put(5, new Mango(5, "Amrapali"));
+		mangoData.put(6, new Mango(6, "Dasheri"));
+		mangoData.put(7, new Mango(7, "Rumani"));
+		mangoData.put(8, new Mango(8, "Himasagar"));
+		mangoData.put(9, new Mango(9, "Raspuri"));
+		mangoData.put(10, new Mango(10, "Kobban"));
+
+
+		return mangoData;
+	}
 }
+
